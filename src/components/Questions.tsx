eb6a02358cff6aba03e2474b7b94551900af4d0a
thsergitox <sergio.pezo.j@uni.pe>
2024-03-03 08:52:13 -0500

    feat: "QR was added"

--- src/components/Questions.tsx
+++ src/components/Questions.tsx
@@ -12,16 +12,16 @@ interface QuestionsProps {
   isVisible: boolean;
   setIsVisible: (isVisible: boolean) => void;
   response: ResponseProps[];
-  setResponse: (response: ResponseProps[]) => void;
   id: string;
 }
 
-const Questions: React.FC<QuestionsProps> = ({ isVisible , setIsVisible, response, setResponse, id})=> {
-  if (!isVisible) return null;
+const Questions: React.FC<QuestionsProps> = ({ isVisible , setIsVisible, response, id})=> {
   
   const handleButtonClick = async () => {
     setIsVisible(true);
   };
+
+  if (!isVisible) return null;
 
   return (
     <Box sx={{display:'flex', flexDirection: 'column', gap:'1rem', bgcolor:'#F7F9FC', borderRadius:'40px', padding:'1rem', paddingLeft:'2rem'}}>
@@ -36,10 +36,15 @@ const Questions: React.FC<QuestionsProps> = ({ isVisible , setIsVisible, respons
           )
         })
       }
-      <Button onClick={handleButtonClick} variant="contained" sx={{bgcolor:'#043C7C', color:'#fff', borderRadius:'20px', marginTop:'1rem', border:'none', width:'200px', alignSelf:'self-start'}}>Crear Quiz</Button>
+      <Typography sx={{mt:'1rem'}} className="title-form">Pase el codigo qr a sus estudiantes</Typography>
+      <div style={{ display: 'flex', justifyContent: 'center' }}>
+        <img src={`https://api.qrserver.com/v1/create-qr-code/?size=150x150&data=http://localhost:5173/questionnaires/${id}`} alt={'Codigo qr generado'} style={{width:'200px'}}/>
+      </div>
+      
       <Link to= {`/questionnaires/${id}`}>
         <Button onClick={handleButtonClick} variant="contained" sx={{bgcolor:'#043C7C', color:'#fff', borderRadius:'20px', marginTop:'1rem', border:'none', width:'200px', alignSelf:'self-start'}}>Ver Quiz</Button>
-       </Link>
+      </Link>
+
     </Box>
   );
 };
